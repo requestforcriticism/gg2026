7f06258079e612d0ff94b33ee8d3cd306bf89ff7
Rogelio Long <rlong021@gmail.com>
2026-05-18 20:48:35 -0600

    Gold node and mouse pointer to 3draycast working

--- scenes/ray_picker_camera/ray_picker_camera.gd
+++ scenes/ray_picker_camera/ray_picker_camera.gd
@@ -12,23 +12,33 @@ var ray_extend := 100.0 #Distance for Raycast to reach level
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var mouse_position: Vector2 = get_viewport().get_mouse_position()
+	#ray_cast_3d.target_position = project_local_ray_normal(mouse_position) * ray_extend
+	
+	# converted that 2d mouse position, into 3d global space, ith a z depth of 0
+	var mouse_position_3d = project_position (mouse_position, 0)
+	# move the raycast object to that position
+	ray_cast_3d.global_position = mouse_position_3d
+	# then cast from where the raycast3d object is, then along the normal
 	ray_cast_3d.target_position = project_local_ray_normal(mouse_position) * ray_extend
+	
 	ray_cast_3d.force_raycast_update()
-	printt(mouse_position,ray_cast_3d.target_position)
+	#printt(mouse_position,ray_cast_3d.global_position,ray_cast_3d.target_position)
 	
 	if ray_cast_3d.is_colliding():
-		if bank.gold >= trap_cost:
-			Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
-			var collider = ray_cast_3d.get_collider()
-			if collider is GridMap:
-				if Input.is_action_pressed("click"):
-					var collision_point = ray_cast_3d.get_collision_point()
-					var cell = gridmap.local_to_map(collision_point)
-					if gridmap.get_cell_item(cell) == 0:
+		var collider = ray_cast_3d.get_collider()
+		if collider is GridMap:
+			var collision_point = ray_cast_3d.get_collision_point()
+			var cell = gridmap.local_to_map(collision_point)
+			print(gridmap.get_cell_item(cell))
+			if gridmap.get_cell_item(cell) == 0:
+				Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+				if Input.is_action_pressed("click"):  #&& trap selected from UI.
+					if bank.gold >= trap_cost:
 						var tile_position = gridmap.map_to_local(cell)
+						print("placing a trap!")
 						#trap_manager.build_turret(tile_position)
 						#bank.gold -= trap_cost
-		else:
-			Input.set_default_cursor_shape(Input.CURSOR_ARROW)
+			else:
+				Input.set_default_cursor_shape(Input.CURSOR_ARROW)
 	else:
 		Input.set_default_cursor_shape(Input.CURSOR_ARROW)
